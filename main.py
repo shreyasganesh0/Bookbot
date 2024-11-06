0d5d843e4c69f529c415e1a37b4781c43431fb24
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-11-06 09:20:43 -0500

    Add function to count_words

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-
+def count_words(text):
+    count = 0
+    for i in text.split():
+        count +=1
+    print(f"count is {count}")
 def main():
     with open("book/frankenstein.txt", 'r') as f:
-        print(f.read())
-
+        file_text=f.read()
+        count_words(file_text)
 
 if __name__ == "__main__":
     main()
