139b00dffae499f342dcfa332403f2c49753fa82
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-11-06 09:30:01 -0500

    Add function to count unique words

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+
+from collections import Counter
+def count_unique_chars(text):
+    text=text.lower()
+    c = Counter(text)
+    print(c)
+    
 def count_words(text):
     count = 0
     for i in text.split():
@@ -7,6 +14,7 @@ def main():
     with open("book/frankenstein.txt", 'r') as f:
         file_text=f.read()
         count_words(file_text)
+        count_unique_chars(file_text)
 
 if __name__ == "__main__":
     main()
