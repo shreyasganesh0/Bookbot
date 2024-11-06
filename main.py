71e8420dcb9a90456222d680e37480ba1ef5781b
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-11-06 10:47:02 -0500

    Add function to create a book report

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-
 from collections import Counter
 def count_unique_chars(text):
-    text=text.lower()
+    text=[i for i in text.lower() if i.isalnum()]
     c = Counter(text)
-    print(c)
+    return c
     
 def count_words(text):
     count = 0
     for i in text.split():
         count +=1
-    print(f"count is {count}")
+    return count
+def book_report(count,words_d):
+    print("---book report for frankenstein---\n")
+    print(f"Number of words {count} \n")
+    for i in words_d:
+        print(f"The count of letter {i} is {words_d[i]}\n")
+    print("---End Report--")
+
+    
 def main():
     with open("book/frankenstein.txt", 'r') as f:
         file_text=f.read()
-        count_words(file_text)
-        count_unique_chars(file_text)
+        word_count=count_words(file_text)
+        word_count_dict=count_unique_chars(file_text)
+        book_report(word_count,word_count_dict)
 
 if __name__ == "__main__":
     main()
